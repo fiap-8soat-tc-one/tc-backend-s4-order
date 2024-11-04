185f9abe852980f3c398d076bd057dc9dda2d9f7
c_jean.silva <c_jean.silva@hapvida.com.br>
2024-11-04 16:49:06 -0300

    feat: add public token

---
+++ src/main/java/com/fiap/tc/infrastructure/core/security/token/CustomerTokenUtil.java
@@ -0,0 +1,41 @@
+﻿package com.fiap.tc.infrastructure.core.security.token;
+
+import com.fiap.tc.domain.entities.Customer;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.oauth2.common.exceptions.UnauthorizedUserException;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+
+import static java.lang.String.format;
+
+@Component
+public class CustomerTokenUtil {
+
+    private final String secretKey = "e0b41325-42e5-4b3a-b995-b05c4e77c56d";
+
+    public String generateToken(Customer customer) {
+        return Jwts.builder()
+                .setSubject(customer.getDocument())
+                .claim("id", customer.getId())
+                .claim("name", customer.getName())
+                .claim("email", customer.getEmail())
+                .claim("document", customer.getDocument())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            throw new UnauthorizedUserException(format("Invalid JWT Token: %s", token));
+        }
+    }
+}
---
+++ src/main/java/com/fiap/tc/infrastructure/presentation/controllers/LoginController.java
@@ -0,0 +1,66 @@
+﻿package com.fiap.tc.infrastructure.presentation.controllers;
+
+import com.fiap.tc.application.usecases.customer.LoadCustomerUseCase;
+import com.fiap.tc.infrastructure.core.security.token.CustomerTokenUtil;
+import com.fiap.tc.infrastructure.presentation.URLMapping;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.common.exceptions.UnauthorizedUserException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.String.format;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping(path = URLMapping.ROOT_PUBLIC_API_AUTH)
+@Api(tags = "token-endpoint", produces = APPLICATION_JSON_VALUE)
+public class LoginController {
+
+    private final LoadCustomerUseCase loadCustomerUseCase;
+    private final CustomerTokenUtil jwtUtil;
+
+    public LoginController(LoadCustomerUseCase loadCustomerUseCase, CustomerTokenUtil jwtUtil) {
+        this.loadCustomerUseCase = loadCustomerUseCase;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, String>> Login(@ApiParam(value = "Document number for login", required = true) @RequestBody Map<String, String> request) {
+
+        String document = request.get("document");
+        Map<String, String> response = new HashMap<>();
+
+        if (document == null || document.isEmpty()) { // anonymous flow
+            response.put("access-token", "");
+            return ok(response);
+        }
+
+        String token = jwtUtil.generateToken(loadCustomerUseCase.load(document));
+
+        response.put("access-token", token);
+
+        return ok(response);
+    }
+
+
+    @PostMapping(path = "/validate", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, String>> ValidateToken(@ApiParam(value = "JWT Token", required = true) @RequestBody Map<String, String> request) {
+
+        String token = request.get("access-token");
+
+        if (token == null || token.isEmpty()) {
+            throw new UnauthorizedUserException(format("Invalid JWT Token: %s", token));
+        }
+
+        jwtUtil.validateToken(token);
+        return ok(null);
+    }
+}
